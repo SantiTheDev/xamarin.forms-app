a3b3168b494f15293bd17e7972e9390a0a0e895b
Santiago Ojeda <ojedasodev@gmail.com>
2022-05-13 11:39:07 -0500

    firts commit

---
+++ README.rd
@@ -0,0 +1,7 @@
+"movil-app"
+
+# movil app with xamain.forms
+
+![alt text](https://github.com/SantiTheDev/xamarin.forms-app/blob/main/image.jpeg?raw=true)
+
+
---
+++ app1/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace app1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void fname_Completed(System.Object sender, System.EventArgs e)
+        {
+            string fname = ((Entry)sender).Text;
+            Console.WriteLine(fname);
+  
+        }
+
+        void fname_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void lname_Completed(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void lname_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+        }
+
+        void email_Completed(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void email_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+        }
+
+        void password_Completed(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void password_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+        }
+
+        void password2_Completed(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        void password2_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+        }
+    }
+}
